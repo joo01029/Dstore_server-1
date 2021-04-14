a0316290e8962b9dc7d00867b38c0f1d5b65a75b
hschan <dlwp7581@gmail.com>
2021-04-14 20:30:47 +0900

    Infinite Scroll

--- src/main/java/gg/jominsubyungsin/service/admin/AdminService.java
+++ src/main/java/gg/jominsubyungsin/service/admin/AdminService.java
@@ -1,4 +1,12 @@
 package gg.jominsubyungsin.service.admin;
 
+import gg.jominsubyungsin.domain.entitiy.UserEntitiy;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
 public interface AdminService {
+    List<UserEntitiy> getUserList(Pageable pageable);
+
 }
