250f352dafa4992b08f4d65abafb0f0fc23009c8
H-SChan <62922765+H-SChan@users.noreply.github.com>
2021-04-20 17:40:38 +0900

    role

--- src/main/java/gg/jominsubyungsin/JominsubyungsinApplication.java
+++ src/main/java/gg/jominsubyungsin/JominsubyungsinApplication.java
@@ -2,12 +2,13 @@ package gg.jominsubyungsin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.servlet.DispatcherServletAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 public class JominsubyungsinApplication {
 
   public static void main(String[] args) {
