5a75e40bb56ec99e2b7e5e8eacfac4a9db0254f3
H-SChan <62922765+H-SChan@users.noreply.github.com>
2021-04-20 17:31:52 +0900

    admin

--- src/main/java/gg/jominsubyungsin/domain/repository/UserRepository.java
+++ src/main/java/gg/jominsubyungsin/domain/repository/UserRepository.java
@@ -2,7 +2,6 @@ package gg.jominsubyungsin.domain.repository;
 
 import gg.jominsubyungsin.domain.entity.UserEntity;
 
-import gg.jominsubyungsin.enums.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
