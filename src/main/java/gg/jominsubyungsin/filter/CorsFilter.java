a0ae0683958b97476e609b36a3a5413418a8997f
joo01029 <62934181+joo01029@users.noreply.github.com>
2021-04-27 20:31:32 +0900

    cors

--- src/main/java/gg/jominsubyungsin/filter/CorsFilter.java
+++ src/main/java/gg/jominsubyungsin/filter/CorsFilter.java
@@ -1,34 +1,34 @@
-//package gg.jominsubyungsin.filter;
-//
-//import lombok.val;
-//import org.apache.catalina.connector.Response;
-//import org.springframework.core.annotation.Order;
-//import org.springframework.stereotype.Component;
-//
-//import javax.persistence.OneToMany;
-//import javax.servlet.*;
-//
-//import javax.servlet.http.HttpServletResponse;
-//import java.io.IOException;
-//
-//
-//
-//@Component
-//@Order(0)
-//public class CorsFilter implements Filter {
-//
-//  @Override
-//  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-//    HttpServletResponse res = (HttpServletResponse) response;
-//
-//    res.setHeader("Access-Control-Allow-Origin","*");
-//    res.setHeader("Access-Control-Allow-Credentials", "true");
-//    res.setHeader("Access-Control-Allow-Methods","POST,GET,PUT,OPTIONS,DELETE,PATCH,HEAD");
-//    res.setHeader("Access-Control-Max-Age","3600");
-//    res.setHeader("Access-Control-Allow-Headers","X-Request-With, Content-Type, Authorization, Origin, Accept, Access-Control-Request-Method, Access-Control-Request-header,Cache-Control, Pragma, Expires");
-//    res.setHeader("Access-Control-Expose-Headers","content-disposition");
-//    System.out.println("cors");
-//    chain.doFilter(request,(ServletResponse) res);
-//  }
-//
-//}
+package gg.jominsubyungsin.filter;
+
+import lombok.val;
+import org.apache.catalina.connector.Response;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.OneToMany;
+import javax.servlet.*;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+
+@Component
+@Order(0)
+public class CorsFilter implements Filter {
+
+  @Override
+  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+    HttpServletResponse res = (HttpServletResponse) response;
+
+    res.setHeader("Access-Control-Allow-Origin","*");
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Methods","POST,GET,PUT,OPTIONS,DELETE,PATCH,HEAD");
+    res.setHeader("Access-Control-Max-Age","3600");
+    res.setHeader("Access-Control-Allow-Headers","X-Request-With, Content-Type, Authorization, Origin, Accept, Access-Control-Request-Method, Access-Control-Request-header,Cache-Control, Pragma, Expires");
+    res.setHeader("Access-Control-Expose-Headers","content-disposition");
+    System.out.println("cors");
+    chain.doFilter(request,response);
+  }
+
+}
