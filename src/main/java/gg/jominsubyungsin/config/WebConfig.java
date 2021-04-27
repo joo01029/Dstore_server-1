e83e3a8984d1718dcff926b837024975a3fc8b01
joo01029 <62934181+joo01029@users.noreply.github.com>
2021-04-27 20:04:39 +0900

    [add] WebConfig

--- src/main/java/gg/jominsubyungsin/config/WebConfig.java
+++ src/main/java/gg/jominsubyungsin/config/WebConfig.java
@@ -2,9 +2,11 @@ package gg.jominsubyungsin.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
   @Override
   public void addCorsMappings(CorsRegistry registry){
@@ -12,6 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
             .allowedOrigins("*")
             .allowedMethods("*")
             .allowedHeaders("*")
+
             .maxAge(3000);
   }
 }
