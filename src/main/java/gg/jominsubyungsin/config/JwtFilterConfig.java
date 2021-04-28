2a6eb5c46adb2b9f8b79848173b434179ec87b57
joo01029 <62934181+joo01029@users.noreply.github.com>
2021-04-28 16:57:47 +0900

    fuck

--- src/main/java/gg/jominsubyungsin/config/JwtFilterConfig.java
+++ src/main/java/gg/jominsubyungsin/config/JwtFilterConfig.java
@@ -1,10 +1,13 @@
 package gg.jominsubyungsin.config;
 
 import gg.jominsubyungsin.filter.JwtAuthorizationFilter;
+import gg.jominsubyungsin.handler.ExeptionHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 
 @Configuration
@@ -13,15 +16,18 @@ public class JwtFilterConfig {
   private HandlerExceptionResolver handlerExceptionResolver;
 
   @Bean
-  public FilterRegistrationBean<JwtAuthorizationFilter> authFilter(){
-    FilterRegistrationBean<JwtAuthorizationFilter> registrationBean = new FilterRegistrationBean<JwtAuthorizationFilter>();
-    registrationBean.setFilter(new JwtAuthorizationFilter());
-    registrationBean.addUrlPatterns("/user/*");
-    registrationBean.addUrlPatterns("/project/create");
-    registrationBean.addUrlPatterns("/project/detail");
-    registrationBean.setOrder(3);
+  public FilterRegistrationBean authFilter(){
+    try {
+      FilterRegistrationBean registrationBean = new FilterRegistrationBean(new JwtAuthorizationFilter());
+      registrationBean.addUrlPatterns("/user/*");
+      registrationBean.addUrlPatterns("/project/create");
+      registrationBean.addUrlPatterns("/project/detail");
+      registrationBean.setOrder(3);
 
-    return registrationBean;
+      return registrationBean;
+    }catch (Exception e){
+      throw new HttpServerErrorException(HttpStatus.INTERNAL_SERVER_ERROR, "fuck cors");
+    }
   }
 
 }
