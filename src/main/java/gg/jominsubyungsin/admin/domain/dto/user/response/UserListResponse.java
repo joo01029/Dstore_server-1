ae7d39a47cac4cd60d9dcfe8929fd7ab7cf16e69
H-SChan <dlwp7581@gmail.com>
2021-05-29 23:35:22 +0900

    h2

--- src/main/java/gg/jominsubyungsin/admin/domain/dto/user/response/UserListResponse.java
+++ src/main/java/gg/jominsubyungsin/admin/domain/dto/user/response/UserListResponse.java
@@ -1,16 +1,17 @@
-package gg.jominsubyungsin.admin.domain.response;
+package gg.jominsubyungsin.admin.domain.dto.user.response;
 
-import gg.jominsubyungsin.admin.domain.dto.query.SelectUserForAdminDto;
+import gg.jominsubyungsin.admin.domain.dto.user.dataIgnore.SelectUserForAdminDto;
 import gg.jominsubyungsin.domain.entity.UserEntity;
 import gg.jominsubyungsin.domain.response.Response;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.domain.Page;
 
 import java.util.List;
 
 @Getter
 @Setter
 public class UserListResponse extends Response {
-    private List<SelectUserForAdminDto> userEntity;
+    private List<SelectUserForAdminDto> users;
     private int totalPages;
 }
--- src/main/java/gg/jominsubyungsin/admin/domain/repository/UserListRepository.java
+++ src/main/java/gg/jominsubyungsin/admin/domain/repository/UserListRepository.java
@@ -1,8 +1,13 @@
 package gg.jominsubyungsin.admin.domain.repository;
 
+import gg.jominsubyungsin.admin.domain.dto.user.dataIgnore.SelectUserForAdminDto;
 import gg.jominsubyungsin.domain.entity.UserEntity;
+import gg.jominsubyungsin.enums.Role;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface UserListRepository extends PagingAndSortingRepository<UserEntity, Long> {
+    public Page<UserEntity> findByRole(Pageable pageable, Role role);
 
 }
